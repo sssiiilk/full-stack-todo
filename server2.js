a0a85b55fd3eb33fd63585a16acdc2fac5e23ed0
Mariya <ms725702@gmail.com>
2026-03-25 18:00:06 +0300

    server2 changes

--- server2.js
+++ server2.js
@@ -1,19 +1,22 @@
-import express from "express";
+// import express from "express";
 
 // const app = express();
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
-});
+// app.get("/health", (req, res) => {
+//   res.status(200).json({ status: "ok" });
+// });
 
-const PORT = process.env.PORT || 8081;
+// const PORT = process.env.PORT || 8081;
 // app.listen(PORT, () => console.log("health server http://localhost:" + PORT + "/health"));
 
 
 
 // const express = require('express');
-const https = require('https');
-const fs = require('fs');
+import express from "express";
+import https from "https";
+import fs from "fs";
+// const https = require('https');
+// const fs = require('fs');
 
 const app = express();
 
